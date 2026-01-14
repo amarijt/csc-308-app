7fe3516d98f5b8ec60ff6d0e8ee37f01b4e4b40d
Amari Thomas <amarijthomas@gmail.com>
2026-01-13 16:57:04 -0800

    Finished step 5

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -1,48 +1,44 @@
-// src/Table.jsx
 import React from "react";
 
-// src/Table.jsx
 function TableHeader() {
   return (
     <thead>
       <tr>
         <th>Name</th>
         <th>Job</th>
+        <th>Remove</th>
       </tr>
     </thead>
   );
 }
 
-function TableBody() {
+function TableBody(props) {
+  const rows = props.characterData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>
+            Delete
+          </button>
+        </td>
+      </tr>
+    );
+  });
+
   return (
     <tbody>
-      <tr>
-        <td>Charli</td>
-        <td>Janitor</td>
-      </tr>
-      <tr>
-        <td>Mac</td>
-        <td>Bouncer</td>
-      </tr>
-      <tr>
-        <td>Dee</td>
-        <td>Aspiring actress</td>
-      </tr>
-      <tr>
-        <td>Dennis</td>
-        <td>Bartender</td>
-      </tr>
+      {rows}
     </tbody>
   );
-
-  
 }
 
-function Table() {
-    return (
-      <table>
+function Table(props) {
+  return (
+     <table>
         <TableHeader />
-        <TableBody />
+        <TableBody characterData={props.characterData} />
       </table>
     );
 }
